cd41d7c14742638ddbd73911d7340481aa17c679
Torino123 <31798044+Torino123@users.noreply.github.com>
2017-09-12 20:31:53 +0900

    Add files via upload
    まだ途中
    Gitの使い方をまだよく把握していない

---
+++ 52-3DDRPG/52-3DDRPG/option.cpp
@@ -0,0 +1,40 @@
+#include "option.h"
+
+void option_c::Ini()
+{
+
+}
+
+void option_c::main()
+{
+	backdraw();
+	itemdraw();
+	configdraw();
+	cursol();
+}
+
+void option_c::backdraw()
+{
+}
+
+void option_c::itemdraw()
+{
+}
+
+void option_c::configdraw()
+{
+	DrawFormatString(240, 20, GetColor(255, 255, 255), "OPTION");
+	DrawFormatString(40, 50, GetColor(255, 255, 255), "BGM");
+	DrawFormatString(40, 90, GetColor(255, 255, 255), "SE");
+	DrawFormatString(40, 130, GetColor(255, 255, 255), "KeyConfig");
+	DrawFormatString(40, 170, GetColor(255, 255, 255), "BattleMode");
+	DrawFormatString(40, 210, GetColor(255, 255, 255), "WindowMode");
+}
+
+void option_c::cursol()
+{
+}
+
+void option_c::Fin()
+{
+}
