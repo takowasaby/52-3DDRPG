283a1dae2604c24b9dcc22dcff4fc63af1984a79
Torino <bamboo.hawk.109@gmail.com>
2017-09-13 10:32:13 +0900

    コメント追加

--- 52-3DDRPG/52-3DDRPG/option.cpp
+++ 52-3DDRPG/52-3DDRPG/option.cpp
@@ -2,11 +2,12 @@
 
 void option_c::Ini()
 {
-	//BGMの音量
-	//SEの音量
+	//BGMの音量大小0-5
+	//SEの音量大小0-5
 	//キーコンフィグ
-	//戦闘モード
-	//ウィンドウモード
+	//戦闘モード0-3
+	//ウィンドウモードtrue or false
+	//背景画像、カーソル画像読み込み
 }
 
 void option_c::main()
@@ -23,16 +24,17 @@ void option_c::backdraw()
 
 void option_c::itemdraw()
 {
-}
-
-void option_c::configdraw()
-{
+	//仮置き項目
 	DrawFormatString(240, 20, GetColor(255, 255, 255), "OPTION");
 	DrawFormatString(40, 50, GetColor(255, 255, 255), "BGM");
 	DrawFormatString(40, 90, GetColor(255, 255, 255), "SE");
 	DrawFormatString(40, 130, GetColor(255, 255, 255), "KeyConfig");
 	DrawFormatString(40, 170, GetColor(255, 255, 255), "BattleMode");
 	DrawFormatString(40, 210, GetColor(255, 255, 255), "WindowMode");
+}
+
+void option_c::configdraw()
+{
 }
 
 void option_c::cursol()
