c1ea16baf13689c8c355305129739e3f30b54f47
Thousan <daichichisaka@gmail.com>
2017-09-20 14:44:51 +0900

    Soundクラス

--- 52-3DDRPG/52-3DDRPG/Sound.cpp
+++ 52-3DDRPG/52-3DDRPG/Sound.cpp
@@ -1,5 +1,13 @@
 #include "define.h"
 
+Sound::Sound() {}
+Sound::~Sound() 
+{
+	InitSoundMem;
+	bn = 0;
+	sn = 0;
+}
+
 void Sound::loadBGM(char BGMgain)
 {
 	BGMproperty[bn].BGM = LoadSoundMem("%c.mp3",BGMgain);
@@ -17,9 +25,10 @@ void Sound::loadSE(char SEgain)
 void Sound::PlayBGM(char BGMgain)
 {
 	for (i = 0; i <= bn; i++){
-		if (BGMgain != BGMproperty[i].BGMname&&CheckSoundMem(BGMproperty[i].BGM) == 1)StopSoundMem(BGMproperty[i].BGM);
-		if (BGMgain == BGMproperty[i].BGMname&&CheckSoundMem(BGMproperty[i].BGM) == 0){
+		if (BGMgain != BGMproperty[i].BGMname && CheckSoundMem(BGMproperty[i].BGM) == 1)StopSoundMem(BGMproperty[i].BGM);
+		if (BGMgain == BGMproperty[i].BGMname && CheckSoundMem(BGMproperty[i].BGM) == 0){
 			PlaySoundMem(BGMproperty[i].BGM, DX_PLAYTYPE_LOOP);
+			ChangeVolumeSoundMem(255 * BGMvolume / 100, BGMproperty[i].BGM);
 			break;
 		}
 	}
@@ -27,16 +36,38 @@ void Sound::PlayBGM(char BGMgain)
 
 void Sound::PlaySE(char SEgain)
 {
-	SEflag2++;
-	for (i = 0; i <= sn; i++)
-		if (SEgain == SEproperty[i].SEname&&SEflag=!SEflag2){
-		PlaySoundMem(SEproperty[i].SE, DX_PLAYTYPE_BACK);
-		SEflag++;
-		break;
+	for (i = 0; i <= sn; i++){
+		if (SEgain == SEproperty[i].SEname) {
+		    PlaySoundMem(SEproperty[i].SE, DX_PLAYTYPE_BACK);
+			ChangeVolumeSoundMem(255 * SEvolume / 100, SEproperty[i].SE);
+			break;
+		}
 	}
 }
 
 void Sound::SoundFinalize()
 {
-	StopSoundMem(quest);
+	InitSoundMem;
+	bn = 0;
+	sn = 0;
+}
+
+void Sound::BGMvolset(int volume) 
+{
+	volume = BGMvolume;
+}
+
+void Sound::SEvolset(int volume)
+{
+	volume = SEvolume;
+}
+
+int Sound::BGMvol() 
+{
+	return BGMvolume;
+}
+
+int Sound::SEvol()
+{
+	return SEvolume;
 }
--- 52-3DDRPG/52-3DDRPG/Sound.h
+++ 52-3DDRPG/52-3DDRPG/Sound.h
@@ -5,30 +5,34 @@ private:
 	int bn = 0;
 	int sn = 0;
 	int i;
-	int SEflag = 0;
-	int SEflag2 = 0;
-	int BGMvolume, SEvolume;
+	int BGMvolume=100;
+	int SEvolume=100;
+
 	typedef struct {
 		int BGM;
 		char BGMname;
 	} BGM;
-	BGM BGMproperty[5];
+	BGM BGMproperty[5];//読み込めるBGMは５つまで
 
 	typedef struct {
 		int SE;
 		char SEname;
 	} SE;
-	SE SEproperty[20];
+	SE SEproperty[20];//読み込めるSEは２０まで
 public:
-	void loadSE(char SEgain);//効果音読み込み
-	void loadBGM(char BGMgain);//BGM読み込み
-	void PlayBGM(char BGMgain);//BGM再生：次or他のBGM再生時のための前BGM停止処理を含む　BGM完全停止は読み込まれてない適当な名前を引数に入れる
-	void PlaySE(char SEgain);//SE再生：SEを同じSE又は別のSEに重ねられるようになっている
-	void SoundFinalize();//全音源のロードアウト
+	Sound();
+	~Sound();
+	void loadSE(char SEgain);//効果音読み込み SEgainに読み込みたいファイル名（mp3のファイル、ファイル名指定にmp3はいらない）
+	void loadBGM(char BGMgain);//BGM読み込み　 BGMgainに読み込みたいファイル名（mp3のファイル、ファイル名指定にmp3はいらない）
+	void PlayBGM(char BGMgain);//BGM再生：次or他のBGM再生時のための前BGM停止処理を含む（新たにファイルを指定すれば前のBGMは止まる）
+	//適当にループに放り込んでも機能するはず
+	//BGM完全停止時は読み込まれてない適当な名前（nullとか）を引数に入れる
+	void PlaySE(char SEgain);//SE再生　特殊な処理なし
+	void SoundFinalize();//全音源のロードアウト　シーン切り替え時のシーン終了処理のときに
 	void BGMvolset(int volume);//設定またはメニューから指定された音量を格納：BGM用
 	void SEvolset(int volume);//SE用
-	int BGMvol();//個別に音量の値を入手したい時用：BGM用
-	int SEvol();//SE用
+	int BGMvol();//個別に音量の値を入手したい時用　音量を返します：BGM用
+	int SEvol();//個別に音量の値を入手したい時用　音量を返します：用
 	//その他、シーン毎に音源読み込みのプリセットがあると便利？
 };;
 
