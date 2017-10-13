f3fb02b39e8249425703f06186698c93acd308d0
Thousan <daichichisaka@gmail.com>
2017-10-13 15:50:49 +0900

    test

--- 52-3DDRPG/52-3DDRPG/main.cpp
+++ 52-3DDRPG/52-3DDRPG/main.cpp
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-#include "define.h"
-
-char Key[256];
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-
-	ChangeWindowMode(true/*option.WinMode*/);//ウィンドウモードで描画
-							//SetGraphMode(WINDOW_X, WINDOW_Y, WINDOW_COLOR_BIT);	// 画面の大きさを変える(640*480の32bit)
-	if (DxLib_Init() == -1) {
-		return -1;
-	}// DXライブラリを初期化処理,エラーが起きたら終了
-
-	if (ChangeWindowMode(TRUE) != DX_CHANGESCREEN_OK || DxLib_Init() == -1) return -1; //初期化処理
-	SetDrawScreen(DX_SCREEN_BACK);        //裏画面に設定
-
-	Control_c control;
-
-	while (!ProcessMessage() && !ClearDrawScreen() && !GetHitKeyStateAll(Key) && !Key[KEY_INPUT_ESCAPE]) {
-		//↑ﾒｯｾｰｼﾞ処理         ↑画面をｸﾘｱ          ↑ｷｰﾎﾞｰﾄﾞ入力状態取得       ↑ESCが押されていない
-
-		control.All();
-
-		ScreenFlip();//裏画面を表画面に反映
-	}
-
-	DxLib_End();
-	return 0;
-}
-=======
 #include "define.h"
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
