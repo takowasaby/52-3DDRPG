bdb17c9d50e5b2af554e522b0288d72e48765e33
kyazuki <2swordstyle.blackblader@gmail.com>
2017-11-04 04:14:58 +0900

    scenario修正2

--- 52-3DDRPG/52-3DDRPG/scenario.cpp
+++ 52-3DDRPG/52-3DDRPG/scenario.cpp
@@ -2,6 +2,8 @@
 
 Scenario_c::Scenario_c() :
 chooseScenario(0),
+first(true),
+end(0),
 bright(0),
 tempScreen(0)
 {
@@ -41,52 +43,59 @@ void Scenario_c::Load()
 
 void Scenario_c::Update()
 {
-	if (mKey[KEY_INPUT_Z] == 1 || mKey[KEY_INPUT_RETURN] == 1) {
-    GMusic.ReserveSound(GMusic.CommonSE[GMusic.DECISION], DX_PLAYTYPE_BACK);
-		GetDrawScreenGraph(0, 0, 640, 480, tempScreen);
-		do {
-			bright = bright - 5;
-			SetDrawBright(bright, bright, bright);
-			ClearDrawScreen();
-			DrawGraph(0, 0, tempScreen, FALSE);
-			ScreenFlip();
-		} while (bright > 0);
-		SetDrawBright(255, 255, 255);
+  if (end == 0) {
+    if (mKey[KEY_INPUT_Z] == 1 || mKey[KEY_INPUT_RETURN] == 1) {
+      GMusic.ReserveSound(GMusic.CommonSE[GMusic.DECISION], DX_PLAYTYPE_BACK);
+      GetDrawScreenGraph(0, 0, 640, 480, tempScreen);
+      end = 1;
+    }
+    else if (mKey[KEY_INPUT_UP] == 1) {
+      if (chooseScenario > 0) { GMusic.ReserveSound(GMusic.CommonSE[GMusic.CURSOR], DX_PLAYTYPE_BACK); chooseScenario--; }
+    }
+    else if (mKey[KEY_INPUT_DOWN] == 1) {
+      if (chooseScenario < SCENARIO_SIZE - 1) { GMusic.ReserveSound(GMusic.CommonSE[GMusic.CURSOR], DX_PLAYTYPE_BACK); chooseScenario++; }
+    }
+    else if (mKey[KEY_INPUT_X] == 1) {
+      GMusic.ReserveSound(GMusic.CommonSE[GMusic.CANCEL], DX_PLAYTYPE_BACK);
+      GetDrawScreenGraph(0, 0, 640, 480, tempScreen);
+      end = -1;
+    }
+  }
 
-		GData.SetScenario(chooseScenario);
-		eventlist->setListFileName(chooseScenario);
-		eventlist->readList();
-		GData.SetEventFlag(1);
-		GData.SceneRequest(0, 0);
-		Reset();
-	}
-	else if (mKey[KEY_INPUT_UP] == 1) {
-    if (chooseScenario > 0) { GMusic.ReserveSound(GMusic.CommonSE[GMusic.CURSOR], DX_PLAYTYPE_BACK); chooseScenario--; }
-	}
-	else if (mKey[KEY_INPUT_DOWN] == 1) {
-    if (chooseScenario < SCENARIO_SIZE - 1) { GMusic.ReserveSound(GMusic.CommonSE[GMusic.CURSOR], DX_PLAYTYPE_BACK); chooseScenario++; }
-	}
-	else if (mKey[KEY_INPUT_X] == 1) {
-		GetDrawScreenGraph(0, 0, 640, 480, tempScreen);
-		do {
-			bright = bright - 5;
-			SetDrawBright(bright, bright, bright);
-			ClearDrawScreen();
-			DrawGraph(0, 0, tempScreen, FALSE);
-			ScreenFlip();
-		} while (bright > 0);
-		SetDrawBright(255, 255, 255);
-
-		Reset();
-		GData.SceneBackRequest();
-	}
+  else {
+    if (bright > 0) {
+      bright -= 5;
+      SetDrawBright(bright, bright, bright);
+      ClearDrawScreen();
+      DrawGraph(0, 0, tempScreen, FALSE);
+      ScreenFlip();
+      SetDrawBright(255, 255, 255);
+      return;
+    }
+    else if (end == 1) {
+      GData.SetScenario(chooseScenario);
+      eventlist->setListFileName(chooseScenario);
+      eventlist->readList();
+      GData.SetEventFlag(1);
+      GData.SceneRequest(0, 0);
+      Reset();
+    }
+    else {
+      Reset();
+      GData.SceneBackRequest();
+    }
+  }
 }
+
 void Scenario_c::Draw()
 {
-	if (bright < 255) {
-		bright = bright + 5;
-		SetDrawBright(bright, bright, bright);
-	}
+  if (first == true) {
+    if (bright < 255) {
+      bright += 5;
+      SetDrawBright(bright, bright, bright);
+    }
+    else first = false;
+  }
 
 	for (int i = 0; i < SCENARIO_SIZE; i++) {
 		if (i == chooseScenario) DrawGraph(0, 96 * i, graph[0][i], TRUE);
